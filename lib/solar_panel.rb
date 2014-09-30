c2c5d1ae50f52448fcf2919ccf2527bb00f1a7b1
Tom <tom@towens.com>
2014-09-30 06:55:21 -0600

    Add thermometer and relay to complete a SolarPanel.

--- lib/solar_panel.rb
+++ lib/solar_panel.rb
@@ -2,8 +2,22 @@ class SolarPanel
 
 	attr_accessor :name
 
-	def initialize
+	def initialize( devices )
 		@name = ""
+		@thermometer = devices[:thermometer]
+		@control = devices[:control]
+	end
+
+	def temperature
+		@thermometer.temperature
+	end
+
+	def control
+		@control.state
+	end
+
+	def control=( state )
+		@control.state = state
 	end
 
 end
