e0a848ee386321a34d190fd84bc007ea48c5dd31
Tom <tom@towens.com>
2014-10-07 17:38:19 -0600

    Why don't these match in git?

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'onewire'
+require './lib/thermometer'
+require './lib/logger'
+
+client = Onewire.client 'towens.com'
+path = client.dir[0]
+
+therm = Thermometer.new client, path
+therm.name = "South Window"
+
+logger = Logger.new
+logger.register therm
+
+logger.run
+sleep 60
+logger.stop
