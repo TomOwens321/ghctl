011f384aea1c8818b552bba6cfcea9cc4636b872
Tom <tom@towens.com>
2014-10-08 08:20:29 -0600

    Fixed merge problems

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'onewire'
 require './lib/thermometer'
 require './lib/logger'
 
-client = Onewire.client 'localhost'
+client = Onewire.client 'towens.com'
 logger = Logger.new
 
 4.times do |l|
@@ -14,8 +14,4 @@ end
 
 logger.run
 sleep 600
-<<<<<<< HEAD
-#logger.stop
-=======
-#logger.stop
->>>>>>> c81b8a515dc39c258c351f9afb334da4e370846a
+
