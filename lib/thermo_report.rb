18ce2c4d5f6738a63e92085c57e6710749f93d83
Tom Owens <tomo@spectralogic.com>
2014-10-17 13:29:13 -0600

    Thermometers now default to Fahrenheit so the log reader no longer needs to provide the conversion.

--- lib/thermo_report.rb
+++ lib/thermo_report.rb
@@ -8,6 +8,6 @@ logFiles.each do |log|
 	puts "Reading #{log}..."
 	puts "#{File.basename(log)} recorded #{parser.temperature_change_count(log, "South Window")} temperature changes"
 	mm = parser.min_max( log )
-	puts "Max = %06.2f | Min = %06.2f" % [ mm[:max] * 9/5 + 32, mm[:min] * 9/5 + 32 ]
-	puts "Current temperature : %06.2f" % [parser.last_temperature( log ).split("|")[2].to_f * 9/5 + 32]
+	puts "Max = %06.2f | Min = %06.2f" % [ mm[:max] , mm[:min] ]
+	puts "Current temperature : %06.2f" % [parser.last_temperature( log ).split("|")[2].to_f]
 end
