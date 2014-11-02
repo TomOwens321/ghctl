4560a176ef848f45d924cec7adb9b9d30d248ed4
Tom <tom@towens.com>
2014-11-02 16:34:31 -0700

    Change thermometer names

--- lib/config.rb
+++ lib/config.rb
@@ -1,4 +1,4 @@
 THERMOMETERS = [
-{:id => "/22.10991A000000", :name => "Ground Loop", :location => "GHouse"},
-{:id => "/22.428322000000", :name => "Ambient Air", :location => "GHouse"}
+{:id => "/22.10991A000000", :name => "Outside Air", :location => "GHouse"},
+{:id => "/22.428322000000", :name => "Inside  Air", :location => "GHouse"}
 ]
