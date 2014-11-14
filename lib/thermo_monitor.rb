8f8573cbfdeeebbd4b701ef02980b3a25a52493a
Tom Owens <tom@greenhousepi.(none)>
2014-11-14 08:20:18 -0700

    Changes to get the hourly log to work correctly.

--- lib/thermo_monitor.rb
+++ lib/thermo_monitor.rb
@@ -27,7 +27,7 @@ class ThermoMonitor
 					begin
 					  temp = t[:thermometer].temperature
 					  next if temp == 32.000
-					  unless t[:lastTemp] == temp || Time.now.strftime("%M") == "00"
+					  if t[:lastTemp] != temp || Time.now.strftime("%M") == "00"
 					    #puts "%-24s : %07.3f : %06.2f" % [t[:thermometer].name,  temp, temp * 9/5 + 32]
 					    log_it(t, temp)
 					    t[:lastTemp] = temp
